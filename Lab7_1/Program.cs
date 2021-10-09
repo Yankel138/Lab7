da320a4674e25f61f3fd60b4f4c376ef2a2dacc9
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-09 09:29:55 +0300

    Добавьте файлы проекта.

---
+++ Lab7_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны двух треугольников(x,y,z):");
+            Console.Write("x1 = ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y1 = ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("z1 = ");
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x2 = ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y2 = ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("z2 = ");
+            double z2 = Convert.ToDouble(Console.ReadLine());
+
+            double s1 = CalcSquare(x1, y1, z1);
+            double s2 = CalcSquare(x2, y2, z2);
+
+
+            if ((x1 + y1 < z1) || (x1 + z1 < y1) || (z1 + y1 < x1))
+            {
+                Console.WriteLine("Ошибка. Ваш первый треугольник вовсе не треугольник!");
+            }
+            else if ((x2 + y2 < z2) || (x2 + z2 < y2) || (z2 + y2 < x2))
+            {
+                Console.WriteLine("Ошибка. Ваш второй треугольник вовсе не треугольник!");
+            }
+
+            else if (s1 > s2)
+            {
+                Console.WriteLine("Площадь первого треугольника ({0:f2}) больше площади второго треугольника ({1:f2}).", s1, s2);
+            }
+            else if (s1 < s2)
+            {
+                Console.WriteLine("Площадь первого треугольника ({0:f2}) меньше площади второго треугольника ({1:f2}).", s1, s2);
+            }
+            else
+            {
+                Console.WriteLine("Площадь первого треугольника равна площади второго треугольника ({0})", s1);
+            }
+            Console.ReadKey();
+        }
+
+        static double CalcSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+
+
+    }
+}
---
+++ Lab7_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ребро куба:");
+            double rebro = Convert.ToDouble(Console.ReadLine());
+            Cube(rebro);
+            Console.ReadKey();
+        }
+
+        static void Cube(double a)
+        {
+            double V = Math.Pow(a, 3);   
+            double S = (Math.Pow(a, 2))*6;
+            Console.WriteLine("Объем куба: {0:f2};\nПлощадь поверхности куба: {1:f2}",V,S);
+
+        }
+    }
+}
