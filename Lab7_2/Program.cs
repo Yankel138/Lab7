33cf7e09b262cd5b28bddf26052854e2de40fbf9
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-09 10:30:20 +0300

    .

--- Lab7_2/Program.cs
+++ Lab7_2/Program.cs
@@ -10,18 +10,17 @@ namespace Lab7_2
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите ребро куба:");
-            double rebro = Convert.ToDouble(Console.ReadLine());
-            Cube(rebro);
-            Console.ReadKey();
+            Cube();
         }
 
-        static void Cube(double a)
+        static void Cube()
         {
-            double V = Math.Pow(a, 3);   
-            double S = (Math.Pow(a, 2))*6;
-            Console.WriteLine("Объем куба: {0:f2};\nПлощадь поверхности куба: {1:f2}",V,S);
-
+            Console.Write("Введите ребро куба:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double V = Math.Pow(a, 3);
+            double S = (Math.Pow(a, 2)) * 6;
+            Console.WriteLine("Объем куба: {0:f2};\nПлощадь поверхности куба: {1:f2}", V, S);
+            Console.ReadKey();
         }
     }
 }
